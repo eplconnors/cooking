53b901cb4148d86bec69da9a824d2933dc61a5f0
EMILY CONNORS <EPLEBEL@GMAIL.COM>
2016-12-21 23:50:26 -0500

    add basic stylying

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   resources :products, only: [:index]
   resource :cart, only: [:show]
   resources :order_items, only: [:create, :update, :destroy]
-  root to: "products#index"
+  root to: "welcome#index"
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
+  get 'contact' => 'welcome#contact'
 
-  get 'welcome/contact'
+   post "order_complete" => "carts#order_complete"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
